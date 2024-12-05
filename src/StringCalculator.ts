d5dc93a761f466daa9b3ac84a31909552a95b9a9
Apurv-Salunke <salunke.apurv7@gmail.com>
2024-12-05 16:38:38 +0530

    test: Handle Negative Numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -7,7 +7,7 @@ export class StringCalculator {
     let delimiter = ",";
     if (numbers.startsWith("//")) {
       const parts = numbers.split("\n");
-      delimiter = parts[0].substring(2); 
+      delimiter = parts[0].substring(2);
       numbers = parts[1];
     }
 
@@ -16,6 +16,10 @@ export class StringCalculator {
       .split(delimiter)
       .map((num) => parseInt(num, 10));
 
+    const negatives = nums.filter((num) => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+    }
     return nums.reduce((sum, current) => sum + current, 0);
   }
 }
--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -32,4 +32,12 @@ describe("StringCalculator", () => {
     expect(calculator.add("//;\n1;2")).to.equal(3);
     expect(calculator.add("//|\n4|5|6")).to.equal(15);
   });
+  it("should throw an exception for negative numbers", () => {
+    expect(() => calculator.add("1,-2,3")).to.throw(
+      "negative numbers not allowed: -2"
+    );
+    expect(() => calculator.add("-1,-2,-3")).to.throw(
+      "negative numbers not allowed: -1,-2,-3"
+    );
+  });
 });
